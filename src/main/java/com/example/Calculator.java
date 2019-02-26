902f50262869a046f82a0f99da4ca81ea7dad057
Raymond Feng <enjoyjava@gmail.com>
2019-02-26 08:23:36 -0800

    chore: customize return value

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -20,6 +20,7 @@ package com.example;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
@@ -28,14 +29,18 @@ import javax.jws.soap.SOAPBinding.Style;
 @SOAPBinding(style = Style.DOCUMENT)
 public interface Calculator {
   @WebMethod
+  @WebResult(name = "value")
   int add(@WebParam(name = "intA") int a, @WebParam(name = "intB") int b);
 
   @WebMethod
+  @WebResult(name = "value")
   int subtract(@WebParam(name = "intA") int a, @WebParam(name = "intB") int b);
 
   @WebMethod
+  @WebResult(name = "value")
   int multiply(@WebParam(name = "intA") int a, @WebParam(name = "intB") int b);
 
   @WebMethod
+  @WebResult(name = "value")
   int divide(@WebParam(name = "intA") int a, @WebParam(name = "intB") int b);
 }
