8b9627e2019c4f060cd8dacab88968fd171d456f
Raymond Feng <enjoyjava@gmail.com>
2019-02-25 21:15:11 -0800

    feat: use WebParam to customize parameter names

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -19,6 +19,7 @@
 package com.example;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
@@ -26,15 +27,15 @@ import javax.jws.soap.SOAPBinding.Style;
 @WebService
 @SOAPBinding(style = Style.DOCUMENT)
 public interface Calculator {
-	@WebMethod
-	int add(int a, int b);
+  @WebMethod
+  int add(@WebParam(name = "intA") int a, @WebParam(name = "intB") int b);
 
-	@WebMethod
-	int subtract(int a, int b);
+  @WebMethod
+  int subtract(@WebParam(name = "intA") int a, @WebParam(name = "intB") int b);
 
-	@WebMethod
-	int multiply(int a, int b);
+  @WebMethod
+  int multiply(@WebParam(name = "intA") int a, @WebParam(name = "intB") int b);
 
-	@WebMethod
-	int divide(int a, int b);
+  @WebMethod
+  int divide(@WebParam(name = "intA") int a, @WebParam(name = "intB") int b);
 }
--- src/main/java/com/example/CalculatorImpl.java
+++ src/main/java/com/example/CalculatorImpl.java
@@ -20,23 +20,27 @@ package com.example;
 
 import javax.jws.WebService;
 
-@WebService(endpointInterface = "com.example.Calculator", name = "Calculator", serviceName = "CalculatorService", portName = "CalculatorPort")
+@WebService(
+    endpointInterface = "com.example.Calculator",
+    name = "Calculator",
+    serviceName = "CalculatorService",
+    portName = "CalculatorPort")
 public class CalculatorImpl implements Calculator {
 
-	public int add(int a, int b) {
-		return a + b;
-	}
+  public int add(int a, int b) {
+    return a + b;
+  }
 
-	public int subtract(int a, int b) {
-		return a - b;
-	}
+  public int subtract(int a, int b) {
+    return a - b;
+  }
 
-	public int multiply(int a, int b) {
-		return a * b;
-	}
+  public int multiply(int a, int b) {
+    return a * b;
+  }
 
-	public int divide(int a, int b) {
-		return a / b;
-	}
+  public int divide(int a, int b) {
+    return a / b;
+  }
 
 }
